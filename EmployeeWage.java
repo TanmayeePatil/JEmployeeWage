dbb71348031aaec9f13f6c5397d13d58ec7d6f91
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-12 07:22:33 +0530

    Added: Calculate Daily Wage of employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,7 +4,12 @@ import java.util.Random;
 public class EmployeeWage{
 
  public static void main (String args[]){
-     System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
+      System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
+
+      //USE CASE 2: Calculate Daily Wages
+       int empWagePerHrs=20;
+       double empSalary=0;
+       int empHrs=0;
 
       //USE CASE 1: Employee Absent or Present using renadom function
       Random myRan=new Random();
@@ -14,10 +19,18 @@ public class EmployeeWage{
       if(myRanVar==0)
        {
          System.out.println("Employee Absent");
+         empHrs=0;
        }
       else
        {
          System.out.println("Employee Present");
+         empHrs=8;
        }
+
+     //Calculating Salary
+     empSalary=empWagePerHrs * empHrs;
+
+     System.out.println("Salary of the employee:" + empSalary);
+
  }
 }
