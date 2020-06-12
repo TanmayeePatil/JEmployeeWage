202706d7788e64450d9a2a0a6ae853cb23a1480e
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-12 08:01:24 +0530

    Added: Calculated 100 hrs salary

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,34 +7,35 @@ public class EmployeeWage{
       System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
 
       int empWagePerHrs=20;
-      int empSalary,TotalEmpSalary=0;
-      int empHrs=0;
+      int empSalary,totalEmpSalary=0;
+      int i=0,empHrs=0,totalHr=0;
       int daysPerMonth=20;
-      int i;
+      int totalMaxHr=100;
 
-    for(i=1;i<=daysPerMonth;i++)
+    while(totalHr <= totalMaxHr)
     {
       Random myRan=new Random();
       int myRanVar=myRan.nextInt(3);
-      System.out.println("Random Integers: "+myRanVar);
+      //System.out.println("Random Integers: "+myRanVar);
 
       switch(myRanVar){
-          case 2 : System.out.println("Employee Present Full Time");
+          case 2 : //System.out.println("Employee Present Full Time");
                           empHrs=12;
                           break;
-          case 1 : System.out.println("Employee Present Part Time");
+          case 1 : //System.out.println("Employee Present Part Time");
                           empHrs=8;
                           break;
-          case 0 : System.out.println("Employee Absent");
+          case 0 : //System.out.println("Employee Absent");
                           empHrs=0;
                           break;
           default : System.out.println("Invalid");
       }
-        empSalary=empWagePerHrs * empHrs;
-        System.out.println("DAY"+i+" : Salary of employee: "+empSalary);
-        TotalEmpSalary=empSalary + TotalEmpSalary;
-     }
+        totalHr=totalHr + empHrs;
+        empSalary=empWagePerHrs * totalHr;
+        totalEmpSalary=empSalary + totalEmpSalary;
+    }
     System.out.println("----------------------------------------------");
-    System.out.println("Total Monthly Salary: "+ TotalEmpSalary);
+    System.out.println("Total Salary for 100 Hours: "+ totalEmpSalary);
+    System.out.println("----------------------------------------------");
  }
 }
