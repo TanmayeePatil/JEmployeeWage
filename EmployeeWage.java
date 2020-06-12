a8f21b79f725946161fbef32e264a3fb0d265919
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-12 06:55:55 +0530

    Added: Employee Attendance uisng random function

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,23 @@
+import java.util.Random;
+
+
 public class EmployeeWage{
 
  public static void main (String args[]){
      System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
+
+      //USE CASE 1: Employee Absent or Present using renadom function
+      Random myRan=new Random();
+      int myRanVar=myRan.nextInt(2);
+      System.out.println("Random Integers: "+myRanVar);
+
+      if(myRanVar==0)
+       {
+         System.out.println("Employee Absent");
+       }
+      else
+       {
+         System.out.println("Employee Present");
+       }
  }
 }
