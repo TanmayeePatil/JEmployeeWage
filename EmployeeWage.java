217491228e2510befb86fe871cd5bdabfebcde7e
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-12 07:40:29 +0530

    Added: Solved using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,12 +6,10 @@ public class EmployeeWage{
  public static void main (String args[]){
       System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
 
-      //USE CASE 2: Calculate Daily Wages
        int empWagePerHrs=20;
        double empSalary=0;
        int empHrs=0;
 
-      //USE CASE 1: Employee Absent or Present using renadom function
       Random myRan=new Random();
       int myRanVar=myRan.nextInt(3);
       System.out.println("Random Integers: "+myRanVar);
